ce46cb10cf116c43f6b09735db3b52865ea33c4a
Eman Tariq <emantariq.1004@gmail.com>
2025-01-06 18:11:38 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <AppBody>
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Chat />} />
+          </Routes>
+        </AppBody>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
